255439deb56510e89d3e5ae0a0119a7fbc18a7f9
Rupam Kundu <rupamkundu1000000@gmail.com>
2024-06-26 19:43:00 +0530

    Modified styles for accommodating the hero area.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,11 +5,11 @@ const Hero: React.FC = () => {
     <section className="bg-white py-12 md:py-20">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
         <div className="w-full text-center md:pr-8">
-          <h1 className="text-4xl md:text-5xl font-bold text-blue-800 mt-20 mb-16">BioMedAI</h1>
-          <p className="text-lg font-bold md:text-3xl text-blue-500 my-20">
+          <h1 className="text-4xl md:text-5xl font-bold text-blue-600 mt-20 mb-16">BioMedAI</h1>
+          <p className="text-lg font-bold md:text-3xl text-blue-400 my-20">
             We are building an AI agent for Biomedicine startups to help them with their journey to success.
           </p>
-          <p className="text-lg font-bold md:text-3xl text-blue-500 mt-20">
+          <p className="text-lg font-bold md:text-3xl text-blue-400 mt-20">
             Ask a question and get an answer from our AI agent.
           </p>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,11 @@ import Hero from "./components/hero";
 import FunctionCalling from "./examples/all/page";
 
 const Home = () => {
-  /*
-  const categories = {
-    "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
-  };
-  */
-
   return (
-    <div>
+    <div className="h-fit">
       <Hero />
       <FunctionCalling />
     </div>
-    /*
-    <main className={styles.main}>
-      <div className={styles.title}>
-        Explore sample apps built with Assistants API
-      </div>
-      <div className={styles.container}>
-        {Object.entries(categories).map(([name, url]) => (
-          <a key={name} className={styles.category} href={`/examples/${url}`}>
-            {name}
-          </a>
-        ))}
-      </div>
-    </main>
-    */
   );
 };
 
